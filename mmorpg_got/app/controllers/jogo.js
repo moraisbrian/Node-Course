dfb0757a10e770abdba97e944e806131f71b8b7a
Brian Leonardo de Morais <morais.brian360@gmail.com>
2019-12-01 10:34:33 -0300

    Aula 99 finalizada

--- mmorpg_got/app/controllers/jogo.js
+++ mmorpg_got/app/controllers/jogo.js
@@ -1,3 +1,14 @@
-module.exports.jogo = function (application, req, res){
-    res.render('jogo');
+module.exports.jogo = function (application, req, res) {
+    if (req.session.autorizado) {
+        res.render('jogo', { img_casa: req.session.casa });
+    }
+    else {
+        res.send('Necessário fazer login');
+    }
+}
+
+module.exports.sair = function (application, req, res) {
+    req.session.destroy(function (err){
+        res.render('index', { validacao: {} });
+    })
 }
