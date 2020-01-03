cadecbfeb83dc9f32e5f8bac0a38125f896c5429
Brian Leonardo de Morais <morais.brian360@gmail.com>
2020-01-03 09:04:29 -0300

    Aula 107 finalizada

--- mmorpg_got/app/models/jogoDAO.js
+++ mmorpg_got/app/models/jogoDAO.js
@@ -55,7 +55,11 @@ JogoDAO.prototype.acao = function (acao) {
 JogoDAO.prototype.getAcoes = function (usuario, res) {
     this._connection.open(function (err, mongoclient) {
         mongoclient.collection("acao", function (err, collection) {
-            collection.find({ usuario: usuario }).toArray(function (err, result) {
+
+            var date = new Date();
+            var momento_atual = date.getDate();
+
+            collection.find({ usuario: usuario, acao_termina_em: {$gt:momento_atual} }).toArray(function (err, result) {
                 res.render('pergaminhos', { acoes: result });
             });
             mongoclient.close();
